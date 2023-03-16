723f98ceec52971c2f9f32aa681bf2d043f11190
Juanan Jiménez <juananjimenez1975@gmail.com>
2023-03-16 11:36:27 +0100

    updated config file

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DEBUG = True
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@localhost:5432/coninfluence'
+SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@localhost:5432/coninfluence_postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #Test database
