1f600424a5f3e26336993cfae984344915a63330
Juanan Jiménez <juananjimenez1975@gmail.com>
2023-03-19 03:44:40 +0100

    error in postgres url

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DEBUG = True
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@ldpg-cg840u1mbg53mc3r0d60-a:5432/coninfluence_postgres'
+SQLALCHEMY_DATABASE_URI = 'postgresql://' + DB_USER + ':' + DB_PASS + '@dpg-cg840u1mbg53mc3r0d60-a:5432/coninfluence_postgres'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 #Test database
